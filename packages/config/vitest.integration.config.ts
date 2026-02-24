74d971b736feeef33ff903bbc46fdf404b01fb3b
Thomas Jiang <thomas@mistle.dev>
2026-02-24 22:11:44 +0800

    chore(config): rename integrations -> integration and update vitest.integration.config.ts

--- packages/config/vitest.integration.config.ts
+++ packages/config/vitest.integration.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["integrations/**/*.test.ts"],
+    include: ["integration/**/*.integration.test.ts"],
   },
 });
