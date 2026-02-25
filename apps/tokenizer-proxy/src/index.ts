ad5e9fd75543258ffbcf17f435fc05ec5944a1bc
Thomas Jiang <thomas@mistle.dev>
2026-02-25 23:24:47 +0800

    chore(data-plane): remove tokenizer-proxy scaffold

--- apps/tokenizer-proxy/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { startServer } from "./server.js";
-
-const port = Number(process.env.PORT ?? "3002");
-
-startServer(port);
-
-console.log("@mistle/tokenizer-proxy listening on port " + port);
