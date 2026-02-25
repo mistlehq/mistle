3c1e66d9336351c344dd10f2683026acc7dfbc64
Thomas Jiang <thomas@mistle.dev>
2026-02-25 22:50:55 +0800

    refactor: remove tunnel-gateway app

--- apps/tunnel-gateway/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { startServer } from "./server.js";
-
-const port = Number(process.env.PORT ?? "3000");
-
-startServer(port);
-
-console.log("@mistle/tunnel-gateway listening on port " + port);
