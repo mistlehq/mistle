c58d67626940341c35e5f12461aaa4d81e6fb583
Thomas Jiang <thomas@mistle.dev>
2026-02-25 23:00:36 +0800

    chore(sandbox-manager): delete sandbox-manager

--- apps/sandbox-manager/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { startServer } from "./server.js";
-
-const port = Number(process.env.PORT ?? "3000");
-
-startServer(port);
-
-console.log("@mistle/sandbox-manager listening on port " + port);
