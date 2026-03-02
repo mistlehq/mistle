c8a7ca6ee4e4abac8dc48f028267e39f404bb229
Thomas Jiang <thomas@mistle.dev>
2026-03-02 16:09:11 +0800

    refactor(data-plane-api): remove redundant trpc contract assertions

--- apps/data-plane-api/src/trpc/router.ts
+++ apps/data-plane-api/src/trpc/router.ts
@@ -1,6 +1,4 @@
-import type { DataPlaneTrpcRouter as DataPlaneTrpcRouterContract } from "@mistle/data-plane-trpc/router";
 import { createDataPlaneTrpcRouter as createDataPlaneTrpcContractRouter } from "@mistle/data-plane-trpc/router";
-import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 
 import { createDataPlaneTrpcRouter as createDataPlaneTrpcAppRouter } from "./base.js";
 import { sandboxInstancesTrpcRouter } from "./routers/sandbox-instances.js";
@@ -11,31 +9,3 @@ export const dataPlaneTrpcRouter = createDataPlaneTrpcContractRouter({
 });
 
 export type DataPlaneTrpcRouter = typeof dataPlaneTrpcRouter;
-
-type RouterInputsImplementContract =
-  inferRouterInputs<DataPlaneTrpcRouter> extends inferRouterInputs<DataPlaneTrpcRouterContract>
-    ? true
-    : never;
-const routerInputsImplementContract: RouterInputsImplementContract = true;
-void routerInputsImplementContract;
-
-type ContractInputsImplementRouter =
-  inferRouterInputs<DataPlaneTrpcRouterContract> extends inferRouterInputs<DataPlaneTrpcRouter>
-    ? true
-    : never;
-const contractInputsImplementRouter: ContractInputsImplementRouter = true;
-void contractInputsImplementRouter;
-
-type RouterOutputsImplementContract =
-  inferRouterOutputs<DataPlaneTrpcRouter> extends inferRouterOutputs<DataPlaneTrpcRouterContract>
-    ? true
-    : never;
-const routerOutputsImplementContract: RouterOutputsImplementContract = true;
-void routerOutputsImplementContract;
-
-type ContractOutputsImplementRouter =
-  inferRouterOutputs<DataPlaneTrpcRouterContract> extends inferRouterOutputs<DataPlaneTrpcRouter>
-    ? true
-    : never;
-const contractOutputsImplementRouter: ContractOutputsImplementRouter = true;
-void contractOutputsImplementRouter;
