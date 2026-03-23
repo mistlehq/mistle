f80a6331e0bf0c68c802508915d890b49ed19efe
Thomas Jiang <thomas@mistle.dev>
2026-03-23 17:47:23 +0800

    feat: add sandbox instance resume endpoint

--- apps/control-plane-api/src/sandbox-instances/index.ts
+++ apps/control-plane-api/src/sandbox-instances/index.ts
@@ -10,6 +10,7 @@ export {
 export { route as createSandboxInstanceConnectionTokenRoute } from "./create-sandbox-instance-connection-token/route.js";
 export { route as listSandboxInstancesRoute } from "./list-sandbox-instances/route.js";
 export { route as getSandboxInstanceRoute } from "./get-sandbox-instance/route.js";
+export { route as resumeSandboxInstanceRoute } from "./resume-sandbox-instance/route.js";
 export { badRequestResponseSchema as SandboxInstancesBadRequestResponseSchema } from "./list-sandbox-instances/schema.js";
 export { NotFoundResponseSchema as SandboxInstancesNotFoundResponseSchema } from "@mistle/http/errors.js";
 export { conflictResponseSchema as SandboxInstancesConflictResponseSchema } from "./create-sandbox-instance-connection-token/schema.js";
