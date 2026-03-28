ca22b09eaec6e586f619576c700b302873ad9ec3
Thomas Jiang <thomas@mistle.dev>
2026-03-29 00:18:40 +0800

    refactor: remove redundant inspect provider helper

--- packages/sandbox/src/index.ts
+++ packages/sandbox/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./types.js";
 export * from "./errors.js";
 export * from "./factory.js";
-export * from "./inspect-provider-state.js";
 export * from "./runtime-env.js";
--- packages/sandbox/src/inspect-provider-state.ts
+++
@@ -1,7 +0,0 @@
-import type { SandboxInspectDisposition, SandboxInspectResult } from "./types.js";
-
-export function classifySandboxInspectProviderState(
-  input: SandboxInspectResult,
-): SandboxInspectDisposition {
-  return input.disposition;
-}
