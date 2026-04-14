b9b9e61e7c8061316a4d11cb64d28ecd471730c9
Thomas Jiang <thomas@mistle.dev>
2026-04-14 19:12:14 +0800

    feat: append session links to jira documents

--- packages/integrations-definitions/src/jira/variants/jira-default/definition.ts
+++ packages/integrations-definitions/src/jira/variants/jira-default/definition.ts
@@ -1,10 +1,12 @@
 import { JiraBaseDefinition, type JiraBaseIntegrationDefinition } from "./base-definition.js";
+import { AppendSessionLinkToJiraDocumentRequestMiddleware } from "./egress-request-middleware.server.js";
 import { exchangeJiraClientCredentials } from "./oauth2-client-credentials.server.js";
 import { JiraWebhookSourceCapability } from "./webhook-source.server.js";
 import { JiraWebhookHandler } from "./webhook.server.js";
 
 export const JiraDefinition: JiraBaseIntegrationDefinition = {
   ...JiraBaseDefinition,
+  egressRequestMiddleware: [AppendSessionLinkToJiraDocumentRequestMiddleware],
   oauth2ClientCredentials: {
     exchangeClientCredentials: exchangeJiraClientCredentials,
   },
