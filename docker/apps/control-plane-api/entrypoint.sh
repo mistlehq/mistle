7d56093a153533182dc3d388a8b1876b86b42e50
Thomas Jiang <thomas@mistle.dev>
2026-04-27 17:16:52 +0800

    refactor: remove api startup migrations

--- docker/apps/control-plane-api/entrypoint.sh
+++ docker/apps/control-plane-api/entrypoint.sh
@@ -6,7 +6,4 @@ if [ ! -f "apps/control-plane-api/dist/index.js" ]; then
   exit 1
 fi
 
-node apps/control-plane-api/dist/scripts/run-control-plane-migrations.js
-node apps/control-plane-api/dist/scripts/run-control-plane-workflow-migrations.js
-
 exec node apps/control-plane-api/dist/index.js
--- docker/apps/data-plane-api/entrypoint.sh
+++ docker/apps/data-plane-api/entrypoint.sh
@@ -6,7 +6,4 @@ if [ ! -f "apps/data-plane-api/dist/index.js" ]; then
   exit 1
 fi
 
-node apps/data-plane-api/dist/scripts/run-data-plane-migrations.js
-node apps/data-plane-api/dist/scripts/run-data-plane-workflow-migrations.js
-
 exec node apps/data-plane-api/dist/index.js
