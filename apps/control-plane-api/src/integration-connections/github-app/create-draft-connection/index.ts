bc34938d979362adf8f850a6b3ba05f5ec742edc
Jonathan Low <jlowhy@gmail.com>
2026-04-28 10:50:35 +0800

    refactor: remove provider draft endpoint wrappers

--- apps/control-plane-api/src/integration-connections/github-app/create-draft-connection/index.ts
+++
@@ -1,8 +0,0 @@
-export { handler } from "./handler.js";
-export { route } from "./route.js";
-export {
-  CreateGitHubAppDraftConnectionBadRequestResponseSchema,
-  CreateGitHubAppDraftConnectionBodySchema,
-  CreateGitHubAppDraftConnectionNotFoundResponseSchema,
-  CreateGitHubAppDraftConnectionParamsSchema,
-} from "./schema.js";
