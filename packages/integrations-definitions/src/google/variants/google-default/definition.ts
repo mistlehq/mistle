9f61c4b0a735fef63a4b25f0af0ce1e24aee75cb
Thomas Jiang <thomas@mistle.dev>
2026-06-24 17:43:08 +0800

    Revert "fix: support google service account capability credentials"
    This reverts commit 1e02a615a08afc6857f51f9df1eb62ede5d4e4a1.

--- packages/integrations-definitions/src/google/variants/google-default/definition.ts
+++ packages/integrations-definitions/src/google/variants/google-default/definition.ts
@@ -3,13 +3,9 @@ import {
   GoogleAuthorizationRevocationCapability,
   GoogleOAuth2AuthorizationCodeCapability,
 } from "./oauth2-authorization-code.server.js";
-import { GoogleServiceAccountCredentialResolver } from "./service-account-credential-resolver.server.js";
 
 export const GoogleDefinition: GoogleBaseIntegrationDefinition = {
   ...GoogleBaseDefinition,
   oauth2AuthorizationCode: GoogleOAuth2AuthorizationCodeCapability,
   authorizationRevocation: GoogleAuthorizationRevocationCapability,
-  credentialResolvers: {
-    default: GoogleServiceAccountCredentialResolver,
-  },
 };
